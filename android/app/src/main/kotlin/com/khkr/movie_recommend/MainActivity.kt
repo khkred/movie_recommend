8a230d19c4d9e4a561273d90a28639219cfbe0ce
Harish Kunchala <124862025+harish-csusb@users.noreply.github.com>
2023-05-05 15:04:42 -0700

    Custom_theme completed

---
+++ android/app/src/main/kotlin/com/khkr/movie_recommend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.khkr.movie_recommend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test/widget_test.dart
@@ -0,0 +1,1 @@
+//TODO: We will add module testers later
